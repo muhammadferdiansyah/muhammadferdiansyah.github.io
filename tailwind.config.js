e69f5025a335a52fe007d735555b058a00c7c2e9
dandyferdiansyah <68523782+muhammadferdiansyah@users.noreply.github.com>
2021-08-23 11:31:48 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+      './*.html',
+      './**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
